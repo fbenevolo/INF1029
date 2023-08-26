48664bfb839caf3a121abaa0af64a84bec44922a
fbenevolo <benevolofelipe@gmail.com>
2023-08-26 15:38:54 -0300

    matrix_lib_test

---
+++ matrix_lib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <matrix_lib.h>
+
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
+
+    return 0;
+}
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC) {
+
+    return 0;
+}
