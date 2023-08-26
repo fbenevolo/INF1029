b7e7afb46bb99efd2834cafe28cb9e43c6c1addf
brunomikmenta <bruno.mikmenta@gmail.com>
2023-08-26 15:59:53 -0300

    update matrix_lib.c, add teste.c

---
+++ matrix_lib.c
@@ -0,0 +1,34 @@
+struct matrix {
+    unsigned long int height;
+    unsigned long int width;
+    float *rows;
+};
+
+
+// Qual seria um caso de erro?
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
+    for(unsigned long int i=0; i<(matrix->height*matrix->width);i++){
+        matrix->rows[i]*=scalar_value;
+    }
+    return 1;
+}
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
+    if(matrixA->width!=matrixB->height){
+        return 0;
+    }
+    //multiplicar elementos da linha de A pelos elementos da coluna de B
+    int n=matrixA->width;
+    int k;
+    for(unsigned long int i=0;i<matrixA->height;i++){
+        for(unsigned long int j=0;j<matrixB->width;j++){
+            k=0;
+            while(k<n){
+                matrixC->rows[i*matrixC->width + j]+= matrixA->rows[i*matrixA->width + k]*matrixB->rows[k*matrixB->width + j];
+                k++;
+            }
+        }
+
+    }
+    return 1;
+}
