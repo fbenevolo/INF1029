5f3479b2091c0423f41d6178bc60fbe001e49336
fbenevolo <benevolofelipe@gmail.com>
2023-08-30 21:42:52 -0300

    atualizacoes-matrix-lib-test

--- matrix_lib_test.c
+++ matrix_lib_test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "time.h"
 #include "matrix_lib.h"
+
+int load_matrix(struct matrix* matrix, FILE* file);
+int store_matrix(struct matrix* matrix, FILE* file);
+int inicialize_matrix(struct matrix* matrix, unsigned long int width, unsigned long int height);
+int inicialize_empty_matrix(struct matrix* matrix, int width, int height);
+int display_matrix(struct matrix* matrix);
 
 int main(int argc, char* argv[]) {
 
@@ -12,27 +19,21 @@ int main(int argc, char* argv[]) {
     // Abrindo arquivos
     file_matrix_A = fopen(&argv[6], "rb");
     file_matrix_B = fopen(&argv[7], "rb");
-    file_result1  = fwrite(&argv[8], "wb");
-    file_result2  = fwrite(&argv[9], "wb");
+    file_result1  = fopen(&argv[8], "wb");
+    file_result2  = fopen(&argv[9], "wb");
 
-    // Preenchendo campos 'linha' e 'coluna' da struct
-    matrixA.height = atoi(argv[2]);
-    matrixA.width  = atoi(argv[3]);
-    matrixB.height = atoi(argv[4]);
-    matrixB.width  = atoi(argv[5]);
-    
-    // Inicializando as matrizes
-    matrixA.rows = (float*)malloc(matrixA.height * matrixA.width * sizeof(float));
-    matrixB.rows = (float*)malloc(matrixB.height * matrixB.width * sizeof(float));
-    if (!matrixA.rows || !matrixB.rows) {
-        fprintf(stderr, "Naoi foi possivel alocar a memoria de uma das matrizes\n");
-        exit(1);
-    }
+    // Inicializando matrizes A e B 
+    inicialize_matrix(matrixA, atoi(argv[2]), atoi(argv[3]));
+    inicialize_matrix(matrixB, atoi(argv[4]), atoi(argv[5]));
 
-    // Preenchendo a matriz em si a partir dos valores lidos do arquivo
-    fread(matrixA.rows, sizeof(float), matrixA.height * matrixA.width, file_matrix_A);
-    fread(matrixB.rows, sizeof(float), matrixB.height * matrixB.width, file_matrix_B);
+    // Inicializando matriz C com colunas de A e linhas de B e preenchendo zeros
+    inicialize_empty_matrix(matrixC, atoi(argv[3]), atoi(argv[4]);)
 
+    // Preenchendo as matrizes A e B
+    load_matrix(matrixA, file_matrix_A);
+    load_matrix(matrixB, file_matrix_B);
+
+    // Fechando arquivos
     fclose(file_matrix_A);
     fclose(file_matrix_B);
     fclose(file_result1);
@@ -40,3 +41,64 @@ int main(int argc, char* argv[]) {
 
    return 0;
 }
+
+/*
+    Inicializa as matrizes, utilizando malloc para o campo 'rows' 
+    e atribuição para os campos 'width' e 'height' 
+*/
+int inicialize_matrix(struct matrix* matrix, unsigned long int width, unsigned long int height) {
+
+    matrix->rows = (float*)malloc*(width * height * sizeof(float));
+    if (!matrix->rows) {
+        fprintf(stderr, "Nao foi possivel alocar a matriz");
+        exit(1);
+    }
+
+    matrix->width = width;
+    matrix->height = height;
+
+    return 1;
+}
+
+
+/* 
+    Carrega na matriz os valores contidos no arquivo binário
+*/
+int load_matrix(struct matrix* matrix, FILE* file) {
+
+    fread(matrix->rows, sizeof(float), matrix->height * matrix->width, file);
+    return 1;
+}
+
+
+/*
+    Guarda os valores da matriz em um arquivo binário
+*/
+int store_matrix(struct matrix* matrix, FILE* filename) {
+
+    return 1;
+}
+
+
+/*
+    Exibe a matriz    
+*/
+int display_matrix(struct matrix* matrix) {
+
+    return 1;
+}
+
+/*
+    Função utilizada apenas para a matriz C
+*/
+int inicialize_empty_matrix(struct matrix* matrix, int width, int height) {
+    matrixC.rows = (float*)malloc(matrixC.width * matrixC.height * sizeof(float))
+    if (!matrixC.rows) {
+        fprintf(stderr, "Nao foi possivel alocar a matriz C");
+        exit(2);
+    }
+
+    for (int i = 0; i < matrixC.width * matrixC.height; i++) matrixC.rows[i] = 0;
+
+    return 1;
+} 
