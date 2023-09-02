facd9897a99dc969b9d9cfc90e665cf42d5e3641
brunomikmenta <bruno.mikmenta@gmail.com>
2023-09-02 16:04:06 -0300

    add matrix_matrix_mult_opt

--- matrix_lib.c
+++ matrix_lib.c
@@ -32,3 +32,28 @@ int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct m
     }
     return 1;
 }
+
+int matrix_matrix_mult_opt(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
+    if(matrixA->width!=matrixB->height){
+        return 0;
+    }
+
+    for(unsigned long int i=0;i<matrixA->height;i++){
+        inicializa_linha(matrixC, i);
+        for(unsigned long int j=0;j<matrixA->width;j++){
+            for(unsigned long int k=0;k<matrixB->width;k++){
+                matrixC->rows[i*matrixC->width + k] += matrixA->rows[i*matrixA->width + j] * matrixB->rows[j*matrixB->width + k];
+            }
+        }
+    }
+
+    return 1; 
+            
+}
+
+void inicializa_linha(struct matrix *matrix, int height){
+    for(int i=0;i<matrix->width;i++){
+        matrix->rows[height*matrix->width + i] = 0;
+    }
+    return;
+}
